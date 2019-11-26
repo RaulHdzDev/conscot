29adfe1744b285b2c1a33ebde87adc8645fdebd2
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2019-11-26 02:15:55 -0600

    comin de usuario y contraseña en el header

--- app/src/main/java/com/example/conscot/Menuslide.java
+++ app/src/main/java/com/example/conscot/Menuslide.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +14,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.conscot.Utilities.SaveSharedPreference;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
@@ -38,6 +40,12 @@ public class Menuslide extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        View head = navigationView.getHeaderView(0);
+        TextView usuario = head.findViewById(R.id.Nombre_usuario);
+        TextView Correo = head.findViewById(R.id.Correo_usuario);
+        usuario.setText(SaveSharedPreference.getCurrentUser(head.getContext()));
+        Correo.setText(SaveSharedPreference.getCurrentEmail(head.getContext()));
     }
 
     @Override
