648e3266ba4350a63eb128c9f56fe850a08ae82e
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2019-12-08 00:32:40 -0600

    fragment categorias modificado, añadir activity materiales construccion, fragment construrama,martinez,dimarsa y view pager adapter

---
+++ app/src/main/java/com/example/conscot/Construcciones/materiales_construccion.java
@@ -0,0 +1,41 @@
+package com.example.conscot.Construcciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.conscot.Fragments_constructoras.Construrama;
+import com.example.conscot.Fragments_constructoras.Dimarsa;
+import com.example.conscot.Fragments_constructoras.Martinez;
+import com.example.conscot.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class materiales_construccion extends AppCompatActivity {
+    private TabLayout tab;
+    private String titles[]={"Construrama","Martinez","Dimarsa"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materiales_construccion);
+        ViewPager viewPager = findViewById(R.id.ViewPager);
+        LoadViewPager(viewPager);
+        tab = findViewById(R.id.Tab);
+        tab.setupWithViewPager(viewPager);
+        TabTitles();
+    }
+    private void LoadViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Construrama());
+        adapter.addFragment(new Martinez());
+        adapter.addFragment(new Dimarsa());
+        viewPager.setAdapter(adapter);
+
+    }
+    private void TabTitles() {
+        for (int i = 0; i<3; i++) {
+            tab.getTabAt(i).setText(titles[i]);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/conscot/Fragments_constructoras/Martinez.java
@@ -0,0 +1,23 @@
+package com.example.conscot.Fragments_constructoras;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.conscot.R;
+
+public class Martinez extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.fragment_construrama, container, false);
+        return v;
+    }
+
+
+}
